466477322c3b8d0fe0c0ce0f20b0ff4cc69c8f48
vanessa <vanessa@soars-MacBook-Pro.local>
2022-04-06 15:10:47 +0800

    a

--- download_quot.php
+++ download_quot.php
@@ -2,5 +2,9 @@
 
 include 'vendor/autoload.php';
 $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('quotation.docx');
-$templateProcessor->setValue('%{name}%', 'vanessa');
-$templateProcessor->saveAs('./vanessa.docx');
+$templateProcessor->setValue('name', $_REQUEST['name']);
+$templateProcessor->setValue('quantity', intval($_REQUEST['quantity']));
+$templateProcessor->setValue('type', $_REQUEST['type']);
+$templateProcessor->setValue('total_prize', 290 * intval($_REQUEST['quantity']));
+$templateProcessor->saveAs('quotation_'.$_REQUEST['name']);
+redirect('quotation_'.$_REQUEST['name']);
