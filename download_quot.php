89d619e39a3d6002b4d69393c528c26d64e71c2e
vanessa <vanessa@soars-MacBook-Pro.local>
2022-04-06 15:16:19 +0800

    a

--- download_quot.php
+++ download_quot.php
@@ -6,5 +6,7 @@ $templateProcessor->setValue('name', $_REQUEST['name']);
 $templateProcessor->setValue('quantity', intval($_REQUEST['quantity']));
 $templateProcessor->setValue('type', $_REQUEST['type']);
 $templateProcessor->setValue('total_prize', 290 * intval($_REQUEST['quantity']));
+echo "start create file\n";
 $templateProcessor->saveAs('quotation_'.$_REQUEST['name']);
-redirect('quotation_'.$_REQUEST['name']);
+echo "start redirect\n";
+echo redirect('quotation_'.$_REQUEST['name']);
